cee7507a4a96c1dc211ae7b312611b7517a60ad2
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-02 17:20:07 -0600

    write out feature tests for adding comments to a specific job

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,14 +11,34 @@ describe "User sees a specific job" do
     expect(page).to have_content(job.level_of_interest)
   end
 
-  xscenario "a user can view the comment form for a specific job" do
+  xscenario "a user can view an existing comment for a specific job" do
+    comment = comment(:create)
+
+    visit company_job_path(comment.job.company, comment.job)
+
+    expect(page).to have_content(comment.body)
+  end
+
+  xscenario "a user can leave and view their comment for a specific job" do
+    job = create(:job)
+
+    comment = create(:comment)
     #assuming a job exists
     # as a user
     # when i navigate to the job show page
+    visit company_job_path(job.company, job)
     # and I fill in some comment in the body
+    fill_in comment.body, with: comment.body
+
     # then I click submit ("Submit Comment")
+
+    click_on "Leave Comment"
+
     # then I am back to the job show page
+
+    expect(current_path).to eq(company_job_path(job.company, job))
     # I can view my comment
+    expect(page).to have_content(comment.body)
   end
 
 end
