5bdc3633ec2e93443a78965d03832293c2ef13c2
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-02 17:02:05 -0600

    add commment route and write out feature test for leaving comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'companies#index'
 
   resources :companies do
-    resources :jobs
+    resources :jobs do
+      resources :comments, only: [:new]
+    end
   end
 
   resources :categories
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -10,4 +10,15 @@ describe "User sees a specific job" do
     expect(page).to have_content(job.title)
     expect(page).to have_content(job.level_of_interest)
   end
+
+  scenario "a user can view the comment form for a specific job" do
+    #assuming a job exists
+    # as a user
+    # when i navigate to the job show page
+    # and I fill in some comment in the body
+    # then I click submit ("Submit Comment")
+    # then I am back to the job show page
+    # I can view my comment 
+  end
+
 end
