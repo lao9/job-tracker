be8f25d65753e8d6fd51914da44c554120c47392
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-03 14:45:18 -0600

    psuedo code feature tests for adding and viewing new contact

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -11,7 +11,24 @@ describe "User sees one company" do
     expect(page).to have_content("ESPN")
     expect(page).to have_content(company.jobs.last.title)
   end
-  xscenario "a user sees the contact information" do
+  xscenario "a user sees existing contact information" do
     # When the user visits the page for a specific Company, in addition to information about the job there is a form that allows them to enter a Contact for that Company (e.g. “Penelope Santorini”, “Hiring Manager”, “penelope@wooo.com”).
+    # Assuming contact information for a specific company already exists
+    # As a user
+    # When I visit a company show page
+    # I can view that contact's name, position, and email
+  end
+  xscenario "a user can add and view new contact information" do
+    # When the user visits the page for a specific Company, in addition to information about the job there is a form that allows them to enter a Contact for that Company (e.g. “Penelope Santorini”, “Hiring Manager”, “penelope@wooo.com”).
+    # As a user
+    # When I visit a company show page
+    # And I fill in contact name
+    # And I fill in contact position
+    # And I fill in contact email
+    # And click "Create New Contact"
+    # I expect to be directed to the company show page
+    # I expect to see the contact's name
+    # I expect to see the contact's position
+    # And I expect to see the contact's email
   end
 end
