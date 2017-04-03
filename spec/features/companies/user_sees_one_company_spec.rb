9e15b5a8b6426406f1e2094d140b0498af14ea2c
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-03 15:31:02 -0600

    clean up feature test comments

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -21,28 +21,21 @@ describe "User sees one company" do
     expect(page).to have_content(contact.email)
   end
   scenario "a user can add and view new contact information" do
-    # When the user visits the page for a specific Company, in addition to information about the job there is a form that allows them to enter a Contact for that Company (e.g. “Penelope Santorini”, “Hiring Manager”, “penelope@wooo.com”).
     contact = build(:contact)
     expect(Contact.count).to eq(0)
     company = create(:company)
-    # As a user
-    # When I visit a company show page
+
     visit company_path(company)
-    # And I fill in contact name
+
     fill_in 'contact[name]', with: contact.name
-    # And I fill in contact position
     fill_in 'contact[position]', with: contact.position
-    # And I fill in contact email
     fill_in 'contact[email]', with: contact.email
-    # And click "Create New Contact"
+
     click_on 'Create New Contact'
-    # I expect to be directed to the company show page
+
     expect(current_path).to eq(company_jobs_path(company))
-    # I expect to see the contact's name
     expect(page).to have_content(contact.name)
-    # I expect to see the contact's position
     expect(page).to have_content(contact.position)
-    # And I expect to see the contact's email
     expect(page).to have_content(contact.email)
   end
 end
