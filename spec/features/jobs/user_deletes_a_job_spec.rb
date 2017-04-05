daa8bb244ee1686ee19667f22834edff0bb70890
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-05 08:37:55 -0600

    smore styling

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -7,7 +7,7 @@ describe "User deletes an existing job" do
     visit company_jobs_path(job.company)
 
     within(".job_#{job.id}") do
-      click_link "Delete"
+      click_button "Delete"
     end
 
     expect(page).to have_content("#{job.title} was successfully deleted!")
